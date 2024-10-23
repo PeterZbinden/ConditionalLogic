975924f141605b181d9e4141f039f1ef932e2db0
Peter Zbinden <peter.zbinden@2prod.ch>
2024-10-23 16:44:30 +0200

    Exposing internal implementations to external components so that Tests and Benchmarks can instantiate the desired implementation without advertising them too much to consumers of the API Surface.

--- Source/Benchmark/LogicGatesBenchmark.cs
+++ Source/Benchmark/LogicGatesBenchmark.cs
@@ -5,6 +5,25 @@ using ConditionalLogic.Gates;
 
 namespace Benchmark;
 
+/// <summary>
+/// Way to access internal Implementations without advertising them to Users of the Library if it were part of the public API-Surface
+/// </summary>
+public class AndAccessor : And
+{
+    public static ExpressionBase<TEvaluationArgs> And2<TEvaluationArgs>(
+        ExpressionBase<TEvaluationArgs> rule1,
+        ExpressionBase<TEvaluationArgs> rule2)
+    {
+        return GetAnd2(rule1, rule2);
+    }
+
+    public static ExpressionBase<TEvaluationArgs> AndList<TEvaluationArgs>(
+        IList<ExpressionBase<TEvaluationArgs>> rules)
+    {
+        return GetAndList(rules);
+    }
+}
+
 public class LogicGatesBenchmark
 {
     public int Count { get; set; } = 1000;
@@ -12,13 +31,13 @@ public class LogicGatesBenchmark
     private const int B = 8;
     private const int C = 999;
 
-    private readonly ExpressionBase<int> _andGate = And.Gate(
+    private readonly ExpressionBase<int> _andGate = AndAccessor.AndList(
         new List<ExpressionBase<int>>
         {
             new EqualsStaticValue<int,int>(x => x, A), 
             new EqualsStaticValue<int,int>(x=>x, B)
         });
-    private readonly ExpressionBase<int> _and2Gate = And.Gate(new EqualsStaticValue<int,int>(x => x, A), new EqualsStaticValue<int,int>(x=>x, B));
+    private readonly ExpressionBase<int> _and2Gate = AndAccessor.And2(new EqualsStaticValue<int,int>(x => x, A), new EqualsStaticValue<int,int>(x=>x, B));
     
     private EqualsStaticValue<int, int> a = new EqualsStaticValue<int, int>(x => x, A);
     private EqualsStaticValue<int, int> b = new EqualsStaticValue<int, int>(x => x, B);
--- Source/ConditionalLogic/Gates/And.cs
+++ Source/ConditionalLogic/Gates/And.cs
@@ -2,7 +2,7 @@
 
 namespace ConditionalLogic.Gates;
 
-public static class And
+public abstract class And
 {
     public static ExpressionBase<TEvaluationArgs> Gate<TEvaluationArgs>(params ExpressionBase<TEvaluationArgs>[] rules)
     {
@@ -26,15 +26,50 @@ public static class And
             case 0:
                 return new False<TEvaluationArgs>();
             case 1:
-                return rules[0];
+                return GetAnd1(rules[0]);
             case 2:
-                return new And2<TEvaluationArgs>(rules[0], rules[1]);
+                return GetAnd2(rules[0], rules[1]);
             case 3:
-                return new And3<TEvaluationArgs>(rules[0], rules[1], rules[2]);
+                return GetAnd3(rules[0], rules[1], rules[2]);
             case 4:
-                return new And4<TEvaluationArgs>(rules[0], rules[1], rules[2], rules[3]);
+                return GetAnd4(rules[0], rules[1], rules[2], rules[3]);
             default:
-                return new AndList<TEvaluationArgs>(rules);
+                return GetAndList(rules);
         }
+    }
+
+    protected static ExpressionBase<TEvaluationArgs> GetAnd1<TEvaluationArgs>(
+        ExpressionBase<TEvaluationArgs> rule)
+    {
+        return rule;
+    }
+
+    protected static ExpressionBase<TEvaluationArgs> GetAnd2<TEvaluationArgs>(
+        ExpressionBase<TEvaluationArgs> rule1, 
+        ExpressionBase<TEvaluationArgs> rule2)
+    {
+        return new And2<TEvaluationArgs>(rule1, rule2);
+    }
+
+    protected static ExpressionBase<TEvaluationArgs> GetAnd3<TEvaluationArgs>(
+        ExpressionBase<TEvaluationArgs> rule1, 
+        ExpressionBase<TEvaluationArgs> rule2, 
+        ExpressionBase<TEvaluationArgs> rule3)
+    {
+        return new And3<TEvaluationArgs>(rule1, rule2, rule3);
+    }
+
+    protected static ExpressionBase<TEvaluationArgs> GetAnd4<TEvaluationArgs>(
+        ExpressionBase<TEvaluationArgs> rule1,
+        ExpressionBase<TEvaluationArgs> rule2,
+        ExpressionBase<TEvaluationArgs> rule3,
+        ExpressionBase<TEvaluationArgs> rule4)
+    {
+        return new And4<TEvaluationArgs>(rule1, rule2, rule3, rule4);
+    }
+
+    protected static ExpressionBase<TEvaluationArgs> GetAndList<TEvaluationArgs>(IList<ExpressionBase<TEvaluationArgs>> rules)
+    {
+        return new AndList<TEvaluationArgs>(rules);
     }
 }
